c95fe6f885b1cd7c52c4d2559c236f89b012c81f
Parth <vermaparth97@gmail.com>
2019-03-18 23:46:54 +0530

    added flask base

---
+++ server.py
@@ -0,0 +1,12 @@
+from elasticsearch import Elasticsearch
+from flask import Flask
+
+es = Elasticsearch()
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+   return "Hello World"
+
+if __name__ == '__main__':
+   app.run(debug=True)
