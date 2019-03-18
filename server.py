daa02df6b1068f7e8532c6b11eebad3dec98b415
Parth <vermaparth97@gmail.com>
2019-03-18 23:53:14 +0530

    added func

--- server.py
+++ server.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
    return "Hello World"
+@app.route('/query1')
+def query1(keywords):
+   return "wait"
 
 if __name__ == '__main__':
    app.run(debug=True)
