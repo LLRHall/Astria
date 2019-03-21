b482659d19aa992caf768fccfee08162701841db
Parth <vermaparth97@gmail.com>
2019-03-21 18:50:08 +0530

    Added search

--- server.py
+++ server.py
@@ -1,15 +1,20 @@
 from elasticsearch import Elasticsearch
 from flask import Flask
 
-es = Elasticsearch()
+es = Elasticsearch("http://localhost:9200")
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
    return "Hello World"
 @app.route('/query1')
-def query1(keywords):
-   return "wait"
+def query1():
+   res = es.search(index="cases",body={"query":{"match":{"legal-key":"Rubber, Credit Note"}}})
+   
+   for cases in res['hits']['hits']:
+      print(cases['_source']['title'])
+
+   return 'OK'
 
 if __name__ == '__main__':
    app.run(debug=True)
