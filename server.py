ded6dee3bb490edabb1a8ad3441b45a7e1c45e5f
Parth <vermaparth97@gmail.com>
2019-03-23 01:02:16 +0530

    added other queries

--- server.py
+++ server.py
@@ -37,7 +37,116 @@ def query1():
            { "match":{"judge":judge}},
            { "match":{"act-used":act}},
            { "match":{"case-cat":cat}},
-           { "range": {"date" : {"gte":"1  January  1900","lte":"1  January  2500", "format":"d  M  y || d  M  y"}}}
+           { "range": {"date" : {"gte":"1 January 1900","lte":"1 January 2500", "format":"d M y || d M y"}}}
+        ]
+    }
+
+   }})
+   print("result is")
+   for cases in res['hits']['hits']:
+      print(cases['_source']['judge'])
+
+   return "Yo"
+
+@app.route('/query2',methods=['GET','POST'])
+def query2():
+   
+   query = request.get_json()
+   # print(query)
+   
+   actquery=query[0]['query']
+   fromDate=query[0]['time']['from']
+   toDate=query[0]['time']['to']
+   cat=query[0]['cat']
+   act=query[0]['act']
+   judge=query[0]['judge']
+   
+   # print(type(keywordsList))
+   # print(toDate)
+   
+   print(actquery[0])
+   # searchString=searchString[:-1]
+   res = es.search(index="acts",
+   body={"query":{
+    "bool": {
+        "must": { "match":{"Act_Name":actquery[0]}},
+    }
+   }})
+
+   file_list=[]
+
+   for acts in res['hits']['hits']:
+      file_list.append(acts['_source']['File_Name'])
+
+   print(file_list)
+   return "Yo"
+
+@app.route('/query3',methods=['GET','POST'])
+def query3():
+   
+   query = request.get_json()
+   # print(query)
+   
+   searchString=query[0]['query']
+   fromDate=query[0]['time']['from']
+   toDate=query[0]['time']['to']
+   cat=query[0]['cat']
+   act=query[0]['act']
+   judge=query[0]['judge']
+   
+   # print(type(keywordsList))
+   # print(toDate)
+   #  = 
+
+   # searchString=searchString[:-1]
+   res = es.search(index="cases",
+   body={"query":{
+   
+    "bool": {
+        "must": { "match":{"title":searchString}},
+        "filter": [
+           { "match":{"judge":judge}},
+           { "match":{"act-used":act}},
+           { "match":{"case-cat":cat}},
+           { "range": {"date" : {"gte":"1 January 1900","lte":"1 January 2500", "format":"d M y || d M y"}}}
+        ]
+    }
+
+   }})
+   print("result is")
+   for cases in res['hits']['hits']:
+      print(cases['_source']['judge'])
+
+   return "Yo"
+
+@app.route('/query4',methods=['GET','POST'])
+def query4():
+   
+   query = request.get_json()
+   # print(query)
+   
+   searchString=query[0]['query']
+   fromDate=query[0]['time']['from']
+   toDate=query[0]['time']['to']
+   cat=query[0]['cat']
+   act=query[0]['act']
+   judge=query[0]['judge']
+   
+   # print(type(keywordsList))
+   # print(toDate)
+   #  = 
+
+   # searchString=searchString[:-1]
+   res = es.search(index="cases",
+   body={"query":{
+   
+    "bool": {
+        "must": { "match":{"title":searchString}},
+        "filter": [
+           { "match":{"judge":judge}},
+           { "match":{"act-used":act}},
+           { "match":{"case-cat":cat}},
+           { "range": {"date" : {"gte":"1 January 1900","lte":"1 January 2500", "format":"d M y || d M y"}}}
         ]
     }
 
