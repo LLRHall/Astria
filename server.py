5d2dc1969c87ebb9e7fe588b8d098da3b43d8bfe
Parth <vermaparth97@gmail.com>
2019-03-22 22:10:19 +0530

    added func

--- server.py
+++ server.py
@@ -1,20 +1,32 @@
 from elasticsearch import Elasticsearch
 from flask import Flask
+import requests
 
 es = Elasticsearch("http://localhost:9200")
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-   return "Hello World"
-@app.route('/query1')
+   return "LLR Open Soft 2019"
+
+@app.route('/query1',methods=['GET','POST'])
 def query1():
-   res = es.search(index="cases",body={"query":{"match":{"legal-key":"Rubber, Credit Note"}}})
    
+   query = requests.get_json()
+   
+   keywordsList = query['query']
+   
+   searchString=""
+   for q in keywordsList:
+      searchString+=q+','
+
+   searchString=searchString[:-1]
+   res = es.search(index="cases",body={"query":{"match":{"legal-key":searchString}}})
+
    for cases in res['hits']['hits']:
       print(cases['_source']['title'])
 
-   return 'OK'
+   return res['hits']['hits']
 
 if __name__ == '__main__':
    app.run(debug=True)
