79ab93bafc58691bdadca6826c9912ff074514da
Parth <vermaparth97@gmail.com>
2019-03-23 18:11:32 +0530

    handles filters, date not working

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 from elasticsearch import Elasticsearch
 from flask import Flask,request
+from flask import jsonify
+
 
 es = Elasticsearch("http://localhost:9200")
 app = Flask(__name__)
@@ -21,48 +23,55 @@ def query1():
    act=query[0]['act']
    judge=query[0]['judge']
    
-   # print(type(keywordsList))
-   # print(toDate)
+   if judge=="":
+      judge="*"
+   if act=="":
+      act="*"
+   if cat=="":
+      cat="*"
+   
    searchString=""
    for q in keywordsList:
       searchString+=q+','
 
    searchString=searchString[:-1]
+   print(judge)
    res = es.search(index="cases",
    body={"query":{
    
     "bool": {
         "must": { "match":{"legal-key":searchString}},
         "filter": [
-           { "match":{"judge":judge}},
-           { "match":{"act-used":act}},
-           { "match":{"case-cat":cat}},
-           { "range": {"date" : {"gte":"1 January 1900","lte":"1 January 2500", "format":"d M y || d M y"}}}
+           { "wildcard":{"judge":{"value":judge}}},
+           { "wildcard":{"act-used":{"value":act}}},
+           { "wildcard":{"case-cat":{"value":cat}}},
         ]
     }
 
    }})
-   print("result is")
-   for cases in res['hits']['hits']:
-      print(cases['_source']['judge'])
-
-   return "Yo"
+   # print("result is")
+   # for cases in res['hits']['hits']:
+   #    print(cases['_source']['judge'])
+   # print(res)
+   return jsonify(res['hits']['hits'])
 
 @app.route('/query2',methods=['GET','POST'])
 def query2():
    
    query = request.get_json()
-   # print(query)
    
    actquery=query[0]['query']
    fromDate=query[0]['time']['from']
    toDate=query[0]['time']['to']
    cat=query[0]['cat']
-   act=query[0]['act']
    judge=query[0]['judge']
    
-   # print(type(keywordsList))
-   # print(toDate)
+   if judge=="":
+      judge="*"
+   if act=="":
+      act="*"
+   if cat=="":
+      cat="*"
    
    print(actquery[0])
    res = es.search(index="acts",
@@ -85,19 +94,25 @@ def query2():
          temp=i[:-5]
       else:
          temp=i[:-4]
-      # print(temp)
+
       res.append(es.search(index="cases",
       body={"query":{
       "bool": {
          "must": { "match":{"filename":temp}},
+         "filter": [
+           { "wildcard":{"judge":{"value":judge}}},
+           { "wildcard":{"act-used":{"value":act}}},
+           { "wildcard":{"case-cat":{"value":cat}}},
+        ]
       }
       }}))
-   
-   for result in res:
-      for cases in result['hits']['hits']:
-         print(cases['_source']['title'])
 
-   return "Yo"
+   resultReturn = []
+   for result in res:
+      resultReturn.append(result['hits']['hits'][0])
+
+   print(resultReturn)
+   return jsonify(resultReturn)
 
 @app.route('/query3',methods=['GET','POST'])
 def query3():
@@ -112,6 +127,12 @@ def query3():
    act=query[0]['act']
    judge=query[0]['judge']
    
+   if judge=="":
+      judge="*"
+   if act=="":
+      act="*"
+   if cat=="":
+      cat="*"
    # print(type(keywordsList))
    # print(toDate)
    #  = 
@@ -123,10 +144,9 @@ def query3():
     "bool": {
         "must": { "match":{"title":searchString}},
         "filter": [
-           { "match":{"judge":judge}},
-           { "match":{"act-used":act}},
-           { "match":{"case-cat":cat}},
-           { "range": {"date" : {"gte":"1 January 1900","lte":"1 January 2500", "format":"d M y || d M y"}}}
+           { "wildcard":{"judge":{"value":judge}}},
+           { "wildcard":{"act-used":{"value":act}}},
+           { "wildcard":{"case-cat":{"value":cat}}},
         ]
     }
 
@@ -150,11 +170,7 @@ def query4():
    act=query[0]['act']
    judge=query[0]['judge']
    
-   # print(type(keywordsList))
-   # print(toDate)
-   #  = 
 
-   # searchString=searchString[:-1]
    res = es.search(index="cases",
    body={"query":{
    
